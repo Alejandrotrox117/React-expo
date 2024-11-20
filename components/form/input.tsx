4bb847829a3b42b557234c35f1fdf5b01c4d1f80
Yorman <yormatkm3@gmail.com>
2024-11-19 23:27:55 -0400

    modo oscuro

--- components/form/input.tsx
+++ components/form/input.tsx
@@ -1,41 +1,55 @@
 import React from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import { TextInput, StyleSheet, useColorScheme } from 'react-native';
 
-type props = {
-    placeholder: string;
-    onChangeText: (text: string) => void;
-    value: string;
-    secureTextEntry?: boolean;
-    style: any;
-    keyboardType?: string;
-    autoCapitalize?: string;
+type Props = {
+  placeholder: string;
+  onChangeText: (text: string) => void;
+  value: string;
+  secureTextEntry?: boolean;
+  style?: any; 
+  keyboardType?: string;
+  autoCapitalize?: string;
+};
 
-}
+const InputField = ({
+  placeholder,
+  onChangeText,
+  value,
+  secureTextEntry,
+  style,
+  keyboardType,
+  autoCapitalize,
+}: Props) => {
+  const colorScheme = useColorScheme(); 
+  const isDarkMode = colorScheme === 'dark';
 
+  // Estilos dinámicos
+  const dynamicStyles = StyleSheet.create({
+    input: {
+      width: '100%',
+      height: 40,
+      borderColor: isDarkMode ? '#fff' : 'gray',
+      backgroundColor: isDarkMode ? '#333' : '#fff',
+      color: isDarkMode ? '#fff' : '#000',
+      borderWidth: 1,
+      borderRadius: 5,
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+  });
 
-const InputField = ({ placeholder, onChangeText, value, secureTextEntry ,style,keyboardType,autoCapitalize}:props) => {
   return (
     <TextInput
-      style={styles.input}
+      style={[dynamicStyles.input, style]} 
       placeholder={placeholder}
+      placeholderTextColor={isDarkMode ? '#ccc' : '#888'} 
       onChangeText={onChangeText}
       value={value}
       secureTextEntry={secureTextEntry}
-     
+      keyboardType={keyboardType as any}
+      autoCapitalize={autoCapitalize as any}
     />
   );
 };
-
-const styles = StyleSheet.create({
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-});
 
 export default InputField;
